da2ef52ca4cf6e5b32f35652c2474fe6876fbf9e
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-06-28 00:33:44 -0300

    algunos cambios en navbar

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Image } from '@chakra-ui/react';
+import { Box, Container, Image, Input, HStack, Text } from '@chakra-ui/react';
 import React from 'react'
 import Logo from '../assets/logo.png'
 
@@ -11,11 +11,31 @@ export const Navbar: React.FC = () => {
                 maxWidth='4xl'
                 display='flex'
                 justifyContent='space-between'
-                padding='2em'
+                alignItems='center'
             >
-                <Image src={Logo} />
-                <div>Search</div>
-                <div>Promo</div>
+                <Box>
+                    <Image src={Logo} paddingBottom='1em' />
+                    <Box>Capital Federal</Box>
+                </Box>
+                <Box fontSize='sm'>
+                    <Box paddingBottom='0.5em'>
+                        <Input placeholder='Buscar productos, marcas y más...' size='md' maxWidth='400px' bg='white' marginX='2em' />
+                    </Box>
+                    <HStack spacing='1em'>
+                        <Box>Categorías</Box>
+                        <Box>Ofertas</Box>
+                        <Box>Historial</Box>
+                        <Box>Supermercado</Box>
+                        <Box>Moda</Box>
+                        <Box>Vender</Box>
+                        <Box>Ayuda</Box>
+                    </HStack>
+                </Box>
+                <Box fontSize='sm'
+                >
+                    <Text paddingBottom={'1em'}>Envíos gratis en 24 hs a partir de $4.000</Text>
+                    <Text>Mi Cuenta</Text>
+                </Box>
             </Container>
         </Box>
     )
