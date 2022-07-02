d9d9c89480e404960090f0f9b400b1852a5f379a
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-07-02 17:24:25 -0300

    navbar

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -8,18 +8,27 @@ export const Navbar: React.FC = () => {
             backgroundColor='yellow.200'
         >
             <Container
-                maxWidth='4xl'
+                maxWidth='container.xl'
                 display='flex'
                 justifyContent='space-between'
                 alignItems='center'
+                paddingY='0.5em'
             >
                 <Box>
                     <Image src={Logo} paddingBottom='1em' />
-                    <Box>Capital Federal</Box>
+                    <Box>
+                        <Text fontSize='xs'>Enviar a</Text>
+                        <Box>Capital Federal</Box>
+                    </Box>
                 </Box>
-                <Box fontSize='sm'>
-                    <Box paddingBottom='0.5em'>
-                        <Input placeholder='Buscar productos, marcas y más...' size='md' maxWidth='400px' bg='white' marginX='2em' />
+                <Box fontSize='sm'
+                    minWidth='50%'
+                >
+                    <Box paddingBottom='2.5em'
+                        display='flex'
+                    >
+                        <Input 
+                        placeholder='Buscar productos, marcas y más...' bg='white' />
                     </Box>
                     <HStack spacing='1em'>
                         <Box>Categorías</Box>
@@ -33,8 +42,15 @@ export const Navbar: React.FC = () => {
                 </Box>
                 <Box fontSize='sm'
                 >
-                    <Text paddingBottom={'1em'}>Envíos gratis en 24 hs a partir de $4.000</Text>
-                    <Text>Mi Cuenta</Text>
+                    <Text paddingBottom={'1em'} fontSize='md'>Envíos gratis en 24 hs a partir de $4.000</Text>
+                    <Box
+                        display='flex'
+                    >
+                        <Text>Creá tu cuenta</Text>
+                        <Text>Ingresá</Text>
+                        <Text>Mis compras</Text>
+                        <span> :+1: </span>
+                    </Box>
                 </Box>
             </Container>
         </Box>
